52d8e494090608f565691a757f04e6437cb72c1a
firstquarterlee <firstquarterlee@gmail.com>
2023-11-10 01:32:43 +0900

    eclipse first commit

---
+++ _ch02/src/VarEx3.java
@@ -0,0 +1,13 @@
+
+public class VarEx3 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		int i = 'A';
+		
+		String str = ""; // 빈문자열(empty string)
+		String str2 = "ABCD";
+		System.out.println(i);
+	}
+
+}
